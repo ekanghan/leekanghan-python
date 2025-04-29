1d6b1fb0aed689bc282fdca1c533a84a89894930
ekanghan <ekanghan1234@gmail.com>
2025-04-29 16:12:23 +0900

    hw7.py

--- hw7.py
+++ hw7.py
@@ -10,12 +10,14 @@ while True:
     name = input("수량은? ")
     shopping_bag.append(item)
     count[item] = name
-    print(f"장바구니에 {item} {name}개가 담겼습니다.")
-    print()
+    print(f"장바구니에 {item} {name}개가 담겼습니다.\n")
 
-print()
-print(f">>>장바구니 보기:{count}")
-print()
-print("[검색]")
+print(f"\n>>>장바구니 보기:{count}")
+print("\n[검색]")
+
 search = input(f"장바구니에서 확인하고자 하는 상품은? ")
-print(f'{search}은(는) {count[search]}개 담겨 있습니다.')
+
+if search in shopping_bag:
+    print(f'{search}은(는) {count[search]}개 담겨 있습니다.')
+else:
+    pass
